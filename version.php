3ee867ad3ba8f425ff7d5bab4fbf1c05a75d1832
githubtrialone <githubtrialone@mailinator.com>
2016-06-29 12:03:35 +0530

    updated version

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 // This is used for display purposes, and also the major/minor/patch
 // numbers are stored in the database and used to determine which sql
 // upgrade file is the starting point for the next upgrade.
-$v_major = '4';
+$v_major = '5';
 $v_minor = '2';
 $v_patch = '1';
 $v_tag   = ''; // minor revision number, should be empty for production releases
